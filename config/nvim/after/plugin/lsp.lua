fd1892f7226e8d85c55590883442d882f3d9ee7c
Casper Ravenhorst <casper@sinedub.io>
2023-06-21 18:04:53 +0200

    feat: clean up lsp.lua and add "diagnostics to qf" shortcut

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -19,20 +19,22 @@ lsp.setup_nvim_cmp({
 	})
 })
 
-lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({buffer = bufnr})
+lsp.on_attach(function(_, bufnr)
+	lsp.default_keymaps({ buffer = bufnr })
 
 	local opts = {buffer = bufnr, remap = false}
 
-	vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+	vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
+	vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
+	vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
+	vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, opts)
+	vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, opts)
+	vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, opts)
+   vim.keymap.set('n', '<leader>dqf', function() vim.diagnostic.setqflist() vim.cmd.copen() end, opts)
+
 	--vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
 	--vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
 end)
 
