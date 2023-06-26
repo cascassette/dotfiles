3dc8dd07a142de42f1a22aae0aa3e641de38c714
Casper Ravenhorst <casper@sinedub.io>
2023-06-26 09:36:32 +0200

    feat: remove TSPlayground

--- config/nvim/lua/cascassette/packer.lua
+++ config/nvim/lua/cascassette/packer.lua
@@ -16,7 +16,7 @@ return require('packer').startup({ function(use)
 
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
-	use 'nvim-treesitter/playground'
+	--use 'nvim-treesitter/playground'
 
 	use 'mbbill/undotree'
 
