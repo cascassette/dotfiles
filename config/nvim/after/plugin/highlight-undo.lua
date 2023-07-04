9e1ba2ba6ae8f283530a39f2a7820c38666ec710
Casper Ravenhorst <casper@sinedub.io>
2023-07-04 14:14:28 +0200

    feat: add highlight-undo plugin

---
+++ config/nvim/after/plugin/highlight-undo.lua
@@ -0,0 +1,9 @@
+require('highlight-undo').setup({
+    --hlgroup = 'HighlightUndo',
+    --duration = 300,
+    duration = 200,
+    --keymaps = {
+      --{'n', 'u', 'undo', {}},
+      --{'n', '<C-r>', 'redo', {}},
+    --}
+})
--- config/nvim/lua/cascassette/packer.lua
+++ config/nvim/lua/cascassette/packer.lua
@@ -45,6 +45,8 @@ return require('packer').startup({ function(use)
    use 'folke/zen-mode.nvim'
    use 'folke/twilight.nvim'
 
+   use 'tzachar/highlight-undo.nvim'
+
    use {
       'nvim-lualine/lualine.nvim',
       requires = { 'nvim-tree/nvim-web-devicons', opt = true }
