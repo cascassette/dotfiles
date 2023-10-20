0fd03a7f01d9ddfa46c6a872ea82d5b60d3a30b6
Casper Ravenhorst <casper@sinedub.io>
2023-10-20 16:08:57 +0200

    feat: add svelte to prettier

--- config/nvim/after/plugin/prettier.lua
+++ config/nvim/after/plugin/prettier.lua
@@ -1,4 +1,5 @@
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-   pattern = { "*.js", "*.jsx", "*.mjs", "*.ts", "*.tsx", "*.css", "*.less", "*.scss", "*.json", "*.graphql", "*.md", "*.vue" },
+   pattern = { "*.js", "*.jsx", "*.mjs", "*.ts", "*.tsx", "*.css", "*.less", "*.scss", "*.json", "*.graphql", "*.md", "*.vue", "*.svelte" },
    command = "PrettierAsync"
 })
+--vim.g.prettier.exec_cmd_path = "~/path/to/cli/prettier"
