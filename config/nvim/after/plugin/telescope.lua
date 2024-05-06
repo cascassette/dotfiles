ba8fa4de728f90b3205b4ed40d2a580f2da241a4
Casper Ravenhorst <ravenc02@heiway.net>
2024-05-06 08:23:28 +0200

    feat: use "smart" send to qflist

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
+local transform_mod = require('telescope.actions.mt').transform_mod
 
 vim.keymap.set('n', '<leader>f', builtin.find_files)
 vim.keymap.set('n', '<leader>g', builtin.git_files)
@@ -16,23 +17,29 @@ end)
 -- search for word under cursor
 vim.keymap.set('n', '°', builtin.grep_string)
 --vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+-- find among sibling files
+vim.keymap.set('n', '<leader>.', function() builtin.find_files({ cwd = vim.fn.expand('%:p:h') }) end)
 
 vim.keymap.set('n', '<leader>c', builtin.git_bcommits, {})
+
+--transform_mod({
+  --open_all_from_qflist = function(prompt_bufnr)
+    --vim.cmd [[silent cfdo e %]]
+  --end
+--})
 
 require('telescope').setup({
    defaults = {
       mappings = {
          i = {
-            -- alt+q sends selected
-            ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
-            -- ctrl+q sends all
-            ['œ'] = actions.send_to_qflist + actions.open_qflist,
+            -- ctrl+q "smart" send to qflist
+            ['<C-q>'] = actions.smart_send_to_qflist + actions.open_qflist,
+            --['œ'] = actions.smart_send_to_qflist + actions.open_all_from_qflist,
          },
          n = {
-            -- alt+q sends selected
-            ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
-            -- ctrl+q sends all
-            ['œ'] = actions.send_to_qflist + actions.open_qflist,
+            -- ctrl+q "smart" send to qflist
+            ['<C-q>'] = actions.smart_send_to_qflist + actions.open_qflist,
+            --['œ'] = actions.smart_send_to_qflist + actions.open_all_from_qflist,
          },
       },
    },
