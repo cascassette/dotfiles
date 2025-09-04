1bacabd3e82d153f1416214ddf08cd69d4a0560e
Casper Ravenhorst <casper@sinedub.io>
2025-09-04 22:36:29 +0200

    feat: tmux-spaces

--- scripts/.scripts/tmux-spaces.sh
+++ scripts/.scripts/tmux-spaces.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
 
-# Old one liner command:
-#find ~/Source/logius -type d -maxdepth 1 | grep -v logius$ | awk 'function basename(dir) { sub(\".*/\", \"\", dir); return dir } {print $0, basename($0)}' | fzf -n 1 --with-nth 2 | ~/open-new-tmux-pane.sh
+CFG_FILE="$HOME/.scripts/spaces.json"
 
-CHOSEN_PROJECT=$(ls ~/.scripts/tmux-spaces/ | fzf)
-if [ -n "$CHOSEN_PROJECT" ]; then
-  ~/.scripts/tmux-spaces/$CHOSEN_PROJECT
+NAME=$(jq -r '.[].name' < $CFG_FILE | fzf)
+if [ -n "$NAME" ]; then
+  SESSION_EXISTS=$(tmux list-sessions)
+  if [[ -n "$SESSION_EXISTS" ]]; then
+    TMUX_COMMAND=new-window
+  else
+    TMUX_COMMAND=new-session
+  fi
+
+  CFG=$(jq -r ".[] | select(.name == \"$NAME\")" < $CFG_FILE)
+  if [[ -z "$CFG" ]]; then exit 404; fi
+  WORKING_DIR=$(echo $CFG | jq -r '.cwd')
+  LAYOUT=$(echo $CFG | jq -r '.layout')
+  COMMAND=$(echo $CFG | jq -r '.command')
+
+  # Check if there is already a window by that name
+  WINDOW_NAME=$NAME
+  WINDOW_EXISTS=$(tmux list-windows -F '#{window_name}' | grep "$WINDOW_NAME")
+  NUMBER=2
+  until [[ -z $WINDOW_EXISTS ]]; do
+    WINDOW_NAME="$NAME$NUMBER"
+    WINDOW_EXISTS=$(tmux list-windows -F '#{window_name}' | grep "$WINDOW_NAME")
+    NUMBER=$(($NUMBER + 1))
+  done
+  tmux TMUX_COMMAND -n $WINDOW_NAME -c $WORKING_DIR $COMMAND
+  if [ "$WINDOW_NAME" = "$NAME" -a "$LAYOUT" = "main-aux" ]; then
+    tmux split-window -t $WINDOW_NAME -l 8 -c $WORKING_DIR
+    tmux select-pane -t $WINDOW_NAME.0
+  fi
 fi
