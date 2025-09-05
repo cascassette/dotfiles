e04ce516b3a4fadac61730c5b485a46345d17521
Casper Ravenhorst <casper@sinedub.io>
2025-09-05 16:41:53 +0200

    fix: remove #lines and #cols from nvim statusline

--- nvim/.config/nvim/after/plugin/mini.statusline.lua
+++ nvim/.config/nvim/after/plugin/mini.statusline.lua
@@ -1,5 +1,10 @@
 require('mini.statusline').setup({})
 
+-- display simple location (line, col)
+require('mini.statusline').section_location = function(args)
+  return '%l│%2v'
+end
+
 -- don't display file size
 require('mini.statusline').section_fileinfo = function(args)
   return string.format('%s %s[%s]', vim.bo.filetype, vim.bo.fileencoding or vim.bo.encoding, vim.bo.fileformat)
