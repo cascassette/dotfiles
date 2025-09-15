17a07633420482fdd00e94bc4639e5dd8a19aa5d
Casper Ravenhorst <casper@sinedub.io>
2025-09-15 20:43:45 +0200

    fix: remove commented lines

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,7 +1,1 @@
---vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
-
---vim.api.nvim_add_user_command('Browse', '!open <q-args>', { nargs = 1 })
--- Not sure why I need this??
---vim.cmd('command! -nargs=1 Browse silent exe \'!open \' . "<args>"')
-
 vim.keymap.set('n', '<leader>z', ':Git difftool<CR>')
