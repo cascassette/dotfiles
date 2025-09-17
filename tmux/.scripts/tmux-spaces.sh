cd9913b543e8ce859eefa4072b6f7e8eb0a7f988
Casper Ravenhorst <casper@sinedub.io>
2025-09-17 20:46:33 +0200

    chore: refactor var names in tmux-spaces script

--- tmux/.scripts/tmux-spaces.sh
+++ tmux/.scripts/tmux-spaces.sh
@@ -7,7 +7,7 @@ FZF_STYLE="fzf --bind tab:toggle-out,shift-tab:toggle-in"
 if [[ "$1" == "DEBUG" ]]; then
   DEBUG=1
 elif [[ -n $(jq -r ".[].name" < $CFG_FILE | grep "$1") ]]; then
-  ARG_NAME="$@"
+  ARG_NAMES="$@"
 fi
 
 # Run with ts DEBUG to do a dry run
@@ -19,23 +19,24 @@ run() {
   fi
 }
 
-if [[ -n "$ARG_NAME" ]]; then
-  NAME="$ARG_NAME"
+if [[ -n "$ARG_NAMES" ]]; then
+  NAMES="$ARG_NAMES"
 else
-  NAME=$(jq -r '.[].name' < $CFG_FILE | $FZF_STYLE -m)
+  NAMES=$(jq -r '.[].name' < $CFG_FILE | $FZF_STYLE -m)
 fi
 
-for SPACE in $NAME; do
-  if [ -n "$SPACE" ]; then
+for NAME in $NAMES; do
+  if [ -n "$NAME" ]; then
     tmux list-sessions >/dev/null 2>&1
     if [[ "$?" != "0" ]]; then
       TMUX_NOT_RUNNING=1
       TMUX_COMMAND="new-session -d -s $SESSION_NAME -x - -y -"
     else
+      TMUX_NOT_RUNNING=
       TMUX_COMMAND=new-window
     fi
 
-    CFG=$(jq -r ".[] | select(.name == \"$SPACE\")" < $CFG_FILE)
+    CFG=$(jq -r ".[] | select(.name == \"$NAME\")" < $CFG_FILE)
     if [[ -z "$CFG" ]]; then
       echo "Config not found" &>2
       exit 404;
@@ -46,22 +47,22 @@ for SPACE in $NAME; do
     AUX_COMMAND=$(echo $CFG | jq -r '.["aux-command"] // empty')
 
     # Check if there is already a window by that name
-    WINDOW_NAME=$SPACE
+    WINDOW_NAME=$NAME
     WINDOW_EXISTS=$(tmux list-windows -F '#{window_name}' 2>/dev/null | grep "$WINDOW_NAME")
     NUMBER=2
     until [[ -z $WINDOW_EXISTS ]]; do
-      WINDOW_NAME="$SPACE$NUMBER"
+      WINDOW_NAME="$NAME$NUMBER"
       WINDOW_EXISTS=$(tmux list-windows -F '#{window_name}' 2>/dev/null | grep "$WINDOW_NAME")
       NUMBER=$(($NUMBER + 1))
     done
 
     # Launch tmux session or create a new window in current session
     run "tmux $TMUX_COMMAND -n $WINDOW_NAME -c $WORKING_DIR $COMMAND"
-    if [ "$WINDOW_NAME" = "$SPACE" -a "$LAYOUT" = "main-aux" ]; then
+    if [ "$WINDOW_NAME" = "$NAME" -a "$LAYOUT" = "main-aux" ]; then
       run "tmux split-window -t $SESSION_NAME:$WINDOW_NAME -l 8 -c $WORKING_DIR $AUX_COMMAND"
       run "tmux select-pane -t $SESSION_NAME:$WINDOW_NAME.0"
     fi
-    if [[ "$TMUX_NOT_RUNNING" ]]; then
+    if [[ -n "$TMUX_NOT_RUNNING" ]]; then
       run "tmux attach -t $SESSION_NAME"
     fi
   fi
