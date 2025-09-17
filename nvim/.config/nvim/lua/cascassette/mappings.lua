0d12d766d9a905cf01df1325f36baae9d172fc50
Casper Ravenhorst <casper@sinedub.io>
2025-09-17 20:45:25 +0200

    feat: add commands to execute lua lines

--- nvim/.config/nvim/lua/cascassette/mappings.lua
+++ nvim/.config/nvim/lua/cascassette/mappings.lua
@@ -24,9 +24,11 @@ vim.keymap.set('n', 'K', vim.cmd.Ouroboros)
 
 vim.keymap.set('n', '<Space>', vim.cmd.write)
 
+-- editing, reloading, executing config
 vim.keymap.set('n', '<leader>ev', ':edit $MYVIMRC<CR>')
 vim.keymap.set('n', '<leader>sv', ':source $MYVIMRC<CR>')
---nnoremap <leader>. :cd %:p:h<CR>
+vim.keymap.set('n', '<leader>x', '<cmd>.lua<cr>', { desc = 'Execute current line of lua' })
+vim.keymap.set('v', '<leader>x', '<cmd>lua<cr>', { desc = 'Execute selected lua code' })
 
 -- disable mouse moving of the cursor
 vim.keymap.set('', '<LeftMouse>', '<nop>')
