a90492ca5592cf73ac3dd8f71c4c2ee27d6fc3ae
Casper Ravenhorst <casper@sinedub.io>
2025-09-26 20:52:22 +0200

    fix: keybind for "hover" in nvim now ,?

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -5,7 +5,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("my.lsp", { clear = true }),
   callback = function()
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to definition" })
-    vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, { desc = "LSP hover definition" })
+    vim.keymap.set('n', '<leader>?', vim.lsp.buf.hover, { desc = "LSP hover definition" })
     vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, { desc = "LSP open float" })
     vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, { desc = "List code actions" })
     vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, { desc = "Find references" })
