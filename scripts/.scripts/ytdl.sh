e40970e52c16a2a4457b2b19933dd2fccf70e258
Casper Ravenhorst <casper@sinedub.io>
2025-09-26 20:55:40 +0200

    fix: yt downloader script

--- scripts/.scripts/ytdl.sh
+++ scripts/.scripts/ytdl.sh
@@ -3,5 +3,5 @@
 while true; do
   read -r -p "Youtube url (enter x to exit): " url
   if [[ "$url" == "x" ]]; then exit 0; fi
-  yt-dlp -x $url
+  yt-dlp --extractor-args "youtube:player-client=default,-tv_simply" -x $url
 done
