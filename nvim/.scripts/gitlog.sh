5d150f0ea76e49d24c82220c033bbdb1c880f72d
Casper Ravenhorst <casper@sinedub.io>
2025-10-01 21:25:39 +0200

    feat: put gitlog.sh output more table-ized for ,c in nvim

--- nvim/.scripts/gitlog.sh
+++ nvim/.scripts/gitlog.sh
@@ -2,4 +2,4 @@
 
 TMP_FILE=/tmp/gitlog
 
-git log --pretty=format:"%h %an %ad %s" --date=relative --abbrev-commit --follow $1 > $TMP_FILE && echo "" >> $TMP_FILE && cat $TMP_FILE
+git log --pretty=format:"%h %<(8,trunc)%an %<(12,trunc)%ad %s" --date=relative --abbrev-commit --follow $1 > $TMP_FILE && echo "" >> $TMP_FILE && cat $TMP_FILE
