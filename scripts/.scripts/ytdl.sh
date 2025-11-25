18799eea05ceb6c8ba1bf801b14ddcedb928b29b
Casper Ravenhorst <casper@sinedub.io>
2025-11-25 09:03:34 +0100

    feat: clearly delineate between last ytdl and the next prompt

--- scripts/.scripts/ytdl.sh
+++ scripts/.scripts/ytdl.sh
@@ -4,4 +4,5 @@ while true; do
   read -r -p "Youtube url (enter x to exit): " url
   if [[ "$url" == "x" ]]; then exit 0; fi
   yt-dlp --extractor-args "youtube:player-client=default,-tv_simply" -x $url
+  echo ==== on to the next ====
 done
