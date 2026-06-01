e6e890385e81e6a55cd9506fa9529d73b465e889
Casper Ravenhorst <casper@sinedub.io>
2026-06-01 19:44:37 +0200

    fix: remove unused flash nvim plugin binds

--- nvim/.config/nvim/lua/cascassette/lazy.lua
+++ nvim/.config/nvim/lua/cascassette/lazy.lua
@@ -132,9 +132,9 @@ require('lazy').setup({
       },
       keys = {
          { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-         { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+         --{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
          --{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-         { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Operator Search" },
+         --{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Operator Search" },
          --{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
       },
    },
